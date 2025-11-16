944f882467b7dd8bcbd658a5f77d069275007649
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:11:56 +0100

    Migrate to new generic AppImage template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,39 +1,19 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
-PACKAGE_BUILDER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/make-aur-package.sh"
+ARCH=$(uname -m)
 
-echo "Installing build dependencies for sharun & AppImage integration..."
+echo "Installing package and its dependencies..."
 echo "---------------------------------------------------------------"
+make-aur-package gapless
 pacman -Syu --noconfirm \
-	base-devel \
-	curl \
-	desktop-file-utils \
-	git \
-	libxtst \
-	wget \
-	xorg-server-xvfb \
-	zsync
-echo "Building the app & it's dependencies..."
-echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$PACKAGE_BUILDER" -O ./make-aur-package.sh
-chmod +x ./make-aur-package.sh
-./make-aur-package.sh gapless
-pacman -Syu --noconfirm \
-	gst-plugins-bad \
-	gst-plugins-base \
-	gst-plugins-good \
-	gst-plugins-ugly \
-	pipewire-jack
+            gst-plugins-bad \
+            gst-plugins-base \
+            gst-plugins-good \
+            gst-plugins-ugly \
+            pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh libxml2-mini mesa-nano gtk4-mini gdk-pixbuf2-mini librsvg-mini opus-mini
-
-echo "Extracting the app version into a version file"
-echo "---------------------------------------------------------------"
-pacman -Q gapless | awk '{print $2; exit}' > ~/version
+get-debloated-pkgs --add-common --prefer-nano
